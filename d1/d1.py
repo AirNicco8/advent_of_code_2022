78915bfd6ecf17328f5d6fa504381814a18151d8
Lorenzo.Niccolai <lorenzo.niccolai@mapsgroup.it>
2022-12-01 16:20:04 +0100

    first day

---
+++ d1/d1.py
@@ -0,0 +1,24 @@
+with open('inputd1.txt', 'r') as f:
+    lines = f.readlines()
+
+a=0
+d={}
+
+for i in lines:
+    if i != '\n':
+        v = int(i.strip())
+        try:
+            d[a].append(v)
+        except:
+            d[a] = [v]
+    else:
+        a+=1
+
+tots = [sum(v) for v in d.values()]
+
+m = max(tots)
+
+tots.sort()
+
+print(m, sum(tots[-3:]))
+        
