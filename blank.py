2055b36ed0c65d00d72f430cffb87da2eb894556
AirNicco8 <39618609+AirNicco8@users.noreply.github.com>
2022-12-03 17:03:18 +0100

    Create blank.py
    blank py template for problems

---
+++ blank.py
@@ -0,0 +1,14 @@
+d = 0 # day problem number
+with open('inputd{}.txt'.format(d), 'r') as f:
+    lines = f.readlines()
+
+# insert problem code here
+    
+print('Part one:')
+print(s)
+
+print('*-'*30)
+
+
+print('Part Two:')
+print(s2)
